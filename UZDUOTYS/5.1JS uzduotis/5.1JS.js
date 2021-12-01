ae21629b25e426931b137a1b155093150273d9f0
gertrudakondrate <gertrudavarnaite@gmail.com>
2021-12-01 16:20:57 +0200

    changes

---
+++ UZDUOTYS/5.1JS uzduotis/5.1JS.js
@@ -0,0 +1,4 @@
+function add(PirmasSkaicius, AntrasSkaicius) {
+    let sum = PirmasSkaicius + AntrasSkaicius;
+    alert(PirmasSkaicius + AntrasSkaicius);
+}
---
+++ classwork/js/pasibandymai.js
@@ -0,0 +1,25 @@
+// apsirasau funkcija kuri bus neveiksmi, kol jos neiskviesim
+function add(number1, number2) {
+    let sum = number1 + number2;
+    alert(number1 + number2);
+}
+
+
+// function getShopingCart() {
+//     return ["Morkos", "Bulves", "Cesnakai"];
+// }
+
+// function fillList(array, targetElement) {
+//     targetElement.innerHTML = "";
+
+
+
+//     for (let index = 0; index < array.length; index++) {
+//         const element = array[index];
+
+//         let li = document.createElement("li"); //sukuriamas elementas
+//         li.innerText = element; //li elementui priskiramas tekstas is masyvo
+
+//         targetElement.append(li); //suformuota li elementa idedame i ul elementa
+//     }
+// }
