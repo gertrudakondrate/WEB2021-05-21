c50971eb7bac3c0442a1235d1c25d5105327ad9a
gertrudakondrate <gertrudavarnaite@gmail.com>
2021-12-06 20:27:25 +0200

    changes

---
+++ UZDUOTYS/6.5/6.5.js
@@ -0,0 +1,7 @@
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function showResult(result, htmlElement) {
+    htmlElement.innerText = result;
+}
---
+++ UZDUOTYS/WEBSITE PROJEKTAS/jswebsite.js
@@ -0,0 +1,9 @@
+function createHeader(headerElement) {
+    let template = '<h1>GERTRUDA VARNAITE <span>PHOTOGRAPHY</span></h1>';
+    headerElement.innerHTML = template;
+}
+
+function createTopNav(topNavelement){
+    let template = '<a class="active" href="index.html">WORK</a> <a href="instagram.html">INSTAGRAM</a> <a href="contact.html">CONTACT</a>';
+topNavelement.innerHTML = template;
+}
